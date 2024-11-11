7862d98ef162d189238b280cf130a8cd7cb4b93a
Thitiporn Panthanan <thitiporn.pah@student.mahidol.ac.th>
2024-11-11 12:48:52 +0700

    submit

--- test_zoo.py
+++ test_zoo.py
@@ -7,8 +7,33 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
-    # Add your additional test cases here.
-
+        
+    # Child test 0-12
+    def test1_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    def test2_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+        
+    # Teen test 13-20
+    def test1_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)  
+    def test2_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)  
+        
+    # Adult test 21-60
+    def test1_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)  
+    def test2_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)  
+        
+    # Elderly test >60
+    def test1_elderly_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)  
+    def test2_elderly_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)     
+          
+    # Invalid age test
+    def test_invalid_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "invalid age")  
 if __name__ == '__main__':
     unittest.main()
--- zoo.py
+++ zoo.py
@@ -1,10 +1,18 @@
 class Zoo:
-    def get_ticket_price(self, age):
-        if 0 < age <= 12:
-            return 50
-        elif 13 <= age < 20:
-            return 100
-        elif 21 < age <= 60:
-            return 150
-        elif age >= 60:
-            return 100
+    def get_ticket_price(self, age): 
+        if 0 <= age <= 12: #s1 {0<=}
+            return 50 #s2
+        elif 13 <= age <= 20: #s3 {<=20}
+            return 100 #s4
+        elif 21 <= age <= 60: #s5 {21<=}
+            return 150 #s6
+        elif age > 60: #s7 {>60}
+            return 100 #s8
+        else: return "invalid age"  # handle invalid
+        
+zoo=Zoo()
+print('test invalid: '+str(zoo.get_ticket_price(-1))) # 'invalid age'
+print('test child: '+str(zoo.get_ticket_price(0))) # expected 50
+print('test teen: '+str(zoo.get_ticket_price(13))) # expected 100
+print('test adult: '+str(zoo.get_ticket_price(21))) # expected 150
+print('test elderly: '+str(zoo.get_ticket_price(61))) # expected 100
